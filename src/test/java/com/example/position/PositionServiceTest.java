8e9657a53542f0386fe831d238c1cc46fcd28c72
Altanis-Wu <Altanis.Wu@gmail.com>
2021-08-23 17:04:13 +0800

    Add unit tests for position.

---
+++ src/test/java/com/example/position/PositionServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.position;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionServiceTest {
+
+    @Test
+    void getPositions() {
+    }
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void addPosition() {
+    }
+
+    @Test
+    void deletePosition() {
+    }
+
+    @Test
+    void updatePosition() {
+    }
+}
---
+++ src/test/java/com/example/position/PositionTest.java
@@ -0,0 +1,48 @@
+package com.example.position;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+
+    @Test
+    void getPositionId() {
+    }
+
+    @Test
+    void setPositionId() {
+    }
+
+    @Test
+    void getSecurity() {
+    }
+
+    @Test
+    void setSecurity() {
+    }
+
+    @Test
+    void getQuantity() {
+    }
+
+    @Test
+    void setQuantity() {
+    }
+
+    @Test
+    void getDatePurchased() {
+    }
+
+    @Test
+    void setDatePurchased() {
+    }
+
+    @Test
+    void getFund() {
+    }
+
+    @Test
+    void setFund() {
+    }
+}

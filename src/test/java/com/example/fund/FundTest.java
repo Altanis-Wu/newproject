0decf6a55b73c3f9e9e952313952b4f2e8b47341
Altanis-Wu <Altanis.Wu@gmail.com>
2021-08-23 09:39:55 +0800

    Add the unit test for fund.

---
+++ src/test/java/com/example/fund/FundTest.java
@@ -0,0 +1,40 @@
+package com.example.fund;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FundTest {
+
+    @Test
+    void getFundId() {
+    }
+
+    @Test
+    void setFundId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getManager() {
+    }
+
+    @Test
+    void setManager() {
+    }
+
+    @Test
+    void getPositions() {
+    }
+
+    @Test
+    void setPositions() {
+    }
+}

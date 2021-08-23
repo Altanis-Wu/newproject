fbd14356f93ddc2ad0166a6990f795cae35806c6
Altanis-Wu <Altanis.Wu@gmail.com>
2021-08-23 17:56:05 +0800

    Fill and finish the unit testing for manager class.

--- src/test/java/com/example/fund/FundControllerTest.java
+++ src/test/java/com/example/fund/FundControllerTest.java
@@ -3,7 +3,6 @@ package com.example.fund;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class FundControllerTest {
 
     @Test
--- src/test/java/com/example/manager/ManagerTest.java
+++ src/test/java/com/example/manager/ManagerTest.java
@@ -1,40 +1,70 @@
 package com.example.manager;
 
+import com.example.fund.Fund;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagerTest {
-
     @Test
     void getEmployeeId() {
+        Manager manager1 = new Manager(0, "Tom", "Yang");
+        Manager manager2 = new Manager(1, "Jerry", "Ye", new ArrayList<Fund>());
+        assertEquals(0, manager1.getEmployeeId());
+        assertEquals(1, manager2.getEmployeeId());
     }
 
     @Test
     void setEmployeeId() {
+        Manager manager = new Manager();
+        manager.setEmployeeId(1);
+        assertEquals(1, manager.getEmployeeId());
     }
 
     @Test
     void getFirstName() {
+        Manager manager1 = new Manager(0, "Tom", "Yang");
+        Manager manager2 = new Manager(1, "Jerry", "Ye", new ArrayList<Fund>());
+        assertEquals(manager1.getFirstName(),"Tom");
+        assertEquals(manager2.getFirstName(), "Jerry");
     }
 
     @Test
     void setFirstName() {
+        Manager manager = new Manager();
+        manager.setFirstName("Tom");
+        assertEquals(manager.getFirstName(),"Tom");
     }
 
     @Test
     void getLastName() {
+        Manager manager1=new Manager(0, "Tom", "Yang");
+        Manager manager2=new Manager(1, "Jerry", "Ye", new ArrayList<Fund>());
+        assertEquals(manager1.getLastName(), "Yang");
+        assertEquals(manager2.getLastName(), "Ye");
     }
 
     @Test
     void setLastName() {
+        Manager manager = new Manager();
+        manager.setLastName("Ye");
+        assertTrue(manager.getLastName().equals("Ye"));
     }
 
     @Test
     void getFunds() {
+        Manager manager = new Manager();
+        Manager manager2=new Manager(1, "Jerry", "Ye", new ArrayList<Fund>());
+        assertTrue(manager.getFunds()==null);
+        assertEquals(0, manager2.getFunds().size());
     }
 
     @Test
     void setFunds() {
+        Manager manager = new Manager();
+        manager.setFunds(new ArrayList<Fund>());
+        assertEquals(0, manager.getFunds().size());
     }
 }

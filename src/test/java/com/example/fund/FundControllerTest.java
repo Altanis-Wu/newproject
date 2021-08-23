64b378de7a080c5df758d78382efcea3c0731790
Altanis-Wu <Altanis.Wu@gmail.com>
2021-08-23 17:10:35 +0800

    Add some test methods to unit tests for fund.

--- src/test/java/com/example/fund/FundControllerTest.java
+++ src/test/java/com/example/fund/FundControllerTest.java
@@ -1,7 +1,28 @@
 package com.example.fund;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FundControllerTest {
 
+    @Test
+    void getFunds() {
+    }
+
+    @Test
+    void getFund() {
+    }
+
+    @Test
+    void addFund() {
+    }
+
+    @Test
+    void deleteFund() {
+    }
+
+    @Test
+    void updateFund() {
+    }
 }
--- src/test/java/com/example/fund/FundServiceTest.java
+++ src/test/java/com/example/fund/FundServiceTest.java
@@ -1,7 +1,28 @@
 package com.example.fund;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FundServiceTest {
 
+    @Test
+    void getFunds() {
+    }
+
+    @Test
+    void getFund() {
+    }
+
+    @Test
+    void addNewFund() {
+    }
+
+    @Test
+    void deleteFund() {
+    }
+
+    @Test
+    void updateFund() {
+    }
 }

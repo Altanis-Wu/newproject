5de814183e2ebeaf3c3a1370203b3681a02d2e85
Altanis-Wu <Altanis.Wu@gmail.com>
2021-08-24 23:07:46 +0800

    Add the unit test for fund.

--- src/test/java/com/example/fund/FundTest.java
+++ src/test/java/com/example/fund/FundTest.java
@@ -1,6 +1,9 @@
 package com.example.fund;
 
+import com.example.manager.Manager;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,33 +11,55 @@ class FundTest {
 
     @Test
     void getFundId() {
+        Fund fund = new Fund(0, "Tech", new Manager());
+        assertEquals(0, fund.getFundId());
     }
 
     @Test
     void setFundId() {
+        Fund fund = new Fund(0, "Tech", new Manager());
+        fund.setFundId(1);
+        assertEquals(1, fund.getFundId());
     }
 
     @Test
     void getName() {
+        Fund fund = new Fund(0, "Tech", new Manager());
+        assertEquals("Tech", fund.getName());
     }
 
     @Test
     void setName() {
+        Fund fund = new Fund(0, "Tech", new Manager());
+        fund.setName("Medi");
+        assertEquals("Medi", fund.getName());
     }
 
     @Test
     void getManager() {
+        Fund fund = new Fund(0, "Tech", new Manager());
+        assertNotNull(fund);
     }
 
     @Test
     void setManager() {
+        Fund fund = new Fund(0, "Tech", new Manager());
+        fund.setManager(new Manager(0, "Larry", "Wu"));
+        assertEquals(0, fund.getManager().getEmployeeId());
     }
 
     @Test
     void getPositions() {
+        Fund fund1 = new Fund(0, "tech", new Manager());
+        assertNull(fund1.getPositions());
+        Fund fund2 = new Fund(1, "Medi", new Manager(), new HashSet<>());
+        assertNotNull(fund2.getPositions());
     }
 
     @Test
     void setPositions() {
+        Fund fund = new Fund(0, "tech", new Manager());
+        fund.setPositions(new HashSet<>());
+        assertNotNull(fund.getPositions());
     }
 }

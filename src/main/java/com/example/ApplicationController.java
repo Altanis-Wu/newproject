4831f9e451d05c550584f3acf9af6883c40280b9
Altanis-Wu <Altanis.Wu@gmail.com>
2021-08-25 16:33:48 +0800

    set prefix.

--- src/main/java/com/example/ApplicationController.java
+++ src/main/java/com/example/ApplicationController.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ApplicationController {
-    @RequestMapping("/")
+    @RequestMapping
     public String index(){
         return "index";
     }

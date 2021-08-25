527a15842da835d745d8590ed5f1cb4d47bb6b00
Altanis-Wu <Altanis.Wu@gmail.com>
2021-08-25 16:24:29 +0800

    Compact url

--- src/main/java/com/example/position/PositionController.java
+++ src/main/java/com/example/position/PositionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/position")
+@RequestMapping("/position")
 public class PositionController {
     private final PositionService positionService;
 
--- src/main/java/com/example/security/SecurityController.java
+++ src/main/java/com/example/security/SecurityController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/security")
+@RequestMapping(path = "/security")
 public class SecurityController {
     private final SecurityService securityService;
 

62ed63f7d245f9d548d24cca1cd617698dd5b5ff
Altanis-Wu <Altanis.Wu@gmail.com>
2021-08-26 10:13:21 +0800

    Use annotation controller instead of restcontroller

--- src/main/java/com/example/ApplicationController.java
+++ src/main/java/com/example/ApplicationController.java
@@ -1,11 +1,9 @@
 package com.example;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class ApplicationController {
     @GetMapping
     public String getIndex(){
